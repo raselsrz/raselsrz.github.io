ccd5ea0bf762889291c2c146e3b84509176720c1
raselsdpm <raselislam4521@gmail.com>
2023-08-29 02:30:33 +0600

    Finished my first ever website

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ Menubtn.addEventListener('click', function(e) {
 /*=============typed=============*/
 
  const typed = new Typed('.typed-text', {
-    strings: ['Web Developer!','Designers!', 'Freelancer!'],
+    strings: ['Web Developer!','Designer!', 'Freelancer!'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 2000,
